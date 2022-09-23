754f53da5fa908c447eca5ce0789b340266a53f2
8JSM <8jadeshape@gmail.com>
2022-09-23 22:09:36 +0900

    2022/09/23/momentum, calculAdd

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 const calculator = {
     plus: function(firstOfNumber, secondOfNumber) {
-        console.log(firstOfNumber + secondOfNumber);
+        return firstOfNumber + secondOfNumber;
     },
     subtraction: function(firstOfNumber, secondOfNumber) {
-        console.log(firstOfNumber - secondOfNumber);
-    },
-    divide: function(firstOfNumber, secondOfNumber) {
-        console.log(firstOfNumber / secondOfNumber);
+        return firstOfNumber - secondOfNumber;
     },
     multiple: function(firstOfNumber, secondOfNumber) {
-        console.log(firstOfNumber ** secondOfNumber);
+        return firstOfNumber * secondOfNumber;
+    },
+    divide: function(firstOfNumber, secondOfNumber) {
+        return firstOfNumber / secondOfNumber;
+    },
+    power: function(firstOfNumber, secondOfNumber) {
+        return firstOfNumber ** secondOfNumber;
     }
 }
 
-calculator.plus(5, 3);
+const plusResult = calculator.plus(5, 3);
+const subtractionResult = calculator.subtraction(plusResult, 5);
+const multipleResult = calculator.multiple(subtractionResult, 2);
+const divideResult = calculator.divide(multipleResult, plusResult);
+const powerResult = calculator.power(plusResult, subtractionResult);
