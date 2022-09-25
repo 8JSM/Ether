822c21d26e5098a344da70b7667564f6c87cc760
8JSM <8jadeshape@gmail.com>
2022-09-25 20:48:05 +0900

    2022/09/25/submitEvent

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-const title = document.querySelector(".hello h1");
+/*const title = document.querySelector(".hello h1");
 
 function handleTitleClick() { 
-    const clicked = "active";
-    if(title.classList.contains(clicked)) {
-        title.classList.remove(clicked);
-    }
-    else {
-        title.classList.add(clicked);
-    }
+    title.classList.toggle("active");
 }
 
 function handleMouseEnter() {
@@ -37,11 +31,9 @@ title.addEventListener("mouseleave",handleMouseLeave);
 
 window.addEventListener("resize",handleWindowResize);
 window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+window.addEventListener("online", handleWindowOnline);**/
 
-
-
-const calculator = {
+/*const calculator = {
     plus: function(firstOfNumber, secondOfNumber) {
         return firstOfNumber + secondOfNumber;
     },
@@ -63,4 +55,26 @@ const plusResult = calculator.plus(5, 3);
 const subtractionResult = calculator.subtraction(plusResult, 5);
 const multipleResult = calculator.multiple(subtractionResult, 2);
 const divideResult = calculator.divide(multipleResult, plusResult);
-const powerResult = calculator.power(plusResult, subtractionResult);
+const powerResult = calculator.power(plusResult, subtractionResult);**/
+
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+/*function loginBtnClick() {
+    const userName = loginInput.value;
+    if(userName === "") {
+        alert("Please write your name");
+    }
+    else if(userName.length > 15) {
+        alert("Your name is too long");
+    }
+}**/
+/*loginButton.addEventListener("click", loginBtnClick);**/
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
