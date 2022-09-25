240bc969c3209ecd10c9224ff6c132d2e11c4751
8JSM <8jadeshape@gmail.com>
2022-09-25 16:14:36 +0900

    2022/09/25/eents

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick() { 
+    const clicked = "active";
+    if(title.classList.contains(clicked)) {
+        title.classList.remove(clicked);
+    }
+    else {
+        title.classList.add(clicked);
+    }
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here";
+
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "beige";
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOOD");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+
+
+
 const calculator = {
     plus: function(firstOfNumber, secondOfNumber) {
         return firstOfNumber + secondOfNumber;
